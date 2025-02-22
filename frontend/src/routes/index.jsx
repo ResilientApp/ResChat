d9f6a5216ef6950517a5655c2da0cbfe51628d25
devangb3 <devangborkar3@gmail.com>
2025-02-22 13:31:30 -0800

    Added all auth pages

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from '../pages/auth/Login.jsx';
+import NewPassword from '../pages/auth/NewPassword.jsx';
+import Register from '../pages/auth/Register.jsx';
+import ResetPassword from '../pages/auth/ResetPassword.jsx';
 const Home = () => <h2>Welcome to ResChat, Development Underway</h2>;
 const About = () => <h2>About</h2>;
 const Contact = () => <h2>Contact</h2>;
@@ -13,6 +16,10 @@ const RoutesComponent = () => {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/newpassword" element={<NewPassword />} />
+        <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/register" element={<Register />}/>
+
       </Routes>
     </Router>
   );

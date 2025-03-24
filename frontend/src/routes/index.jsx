18deb93e9d2b73c607eeac4139700b379d64a8b1
devangb3 <devangborkar3@gmail.com>
2025-03-24 14:35:27 -0700

    Fix default route

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -6,37 +6,36 @@ import Login from '../pages/auth/Login.jsx';
 import NewPassword from '../pages/auth/NewPassword.jsx';
 import Register from '../pages/auth/Register.jsx';
 import ResetPassword from '../pages/auth/ResetPassword.jsx';
-import Settings from '../pages/dashboard/Settings'
-import Profile from '../pages/dashboard/Profile'
-import GeneralApp from '../pages/dashboard/GeneralApp'
-import Error404 from '../pages/Error404.jsx'
+import Settings from '../pages/dashboard/Settings';
+import Profile from '../pages/dashboard/Profile';
+import GeneralApp from '../pages/dashboard/GeneralApp';
+import Error404 from '../pages/Error404.jsx';
 
-const Home = () => <h2>Welcome to ResChat, Development Underway</h2>;
-const About = () => <h2>About</h2>;
-const Contact = () => <h2>Contact</h2>;
+
 
 export default function RoutesComponent() {
   return useRoutes([
+    { path: '/', element: <Navigate to="/auth/login" replace /> },
     {
       path: '/auth',
-      element: <MainLayout/>,
-      children:[
-        {element: <Login/>, path:'login'},
-        {element: <Register/>, path:'register'},
-        {element: <ResetPassword/>, path:'resetpassword'},
-        {element: <NewPassword/>, path:'newpassword'},
+      element: <MainLayout />,
+      children: [
+        { element: <Login />, path: 'login' },
+        { element: <Register />, path: 'register' },
+        { element: <ResetPassword />, path: 'resetpassword' },
+        { element: <NewPassword />, path: 'newpassword' },
       ]
     },
     {
       path: "/",
       element: <DashboardLayout />,
       children: [
-        {element: <GeneralApp/>, path:'app'},
-        {element: <Settings/>, path:'settings'},
-        {element: <Profile/>, path:'profile'},
-        {element: <Error404/>, path:'404'},
-        {element: <Navigate to ="/404" replace />, path:"*" }
-      ],
-    }
-  ]); 
+        { element: <GeneralApp />, path: 'app' },
+        { element: <Settings />, path: 'settings' },
+        { element: <Profile />, path: 'profile' },
+        { element: <Error404 />, path: '404' }
+      ]
+    },
+    { path: "*", element: <Navigate to="/auth/login" replace /> }
+  ]);
 }
